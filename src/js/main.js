33985703770f62914dd6b82122cbf5cd952019b0
Patrick R <gnss1985@gmail.com>
2023-03-22 14:51:48 +0100

    Update index.html, main.js, and style.scss

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,29 @@
-import './../scss/style.scss'
+import "./../scss/style.scss";
+
+window.onload = function () {
+  noflow();
+};
+
+const button = document.querySelector(".modal-button");
+
+button.addEventListener("click", () => {
+  off();
+});
+
+button.addEventListener("click", () => {
+  haveflow();
+});
+
+function off() {
+  document.querySelector(".modal").style.display = "none";
+}
+
+function noflow() {
+  document.body.style.overflow = "hidden";
+  document.body.style.userSelect = "none";
+}
+
+function haveflow() {
+  document.body.style.overflow = "auto";
+  document.body.style.userSelect = "auto";
+}
