cab35305e8598c2ac08bf4358d3ebfa7e4e46375
Patrick R <gnss1985@gmail.com>
2023-03-23 08:47:03 +0100

    Update adopt.html, index.html, and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    root: "src",
-    assetsInclude: ["./assets/**/*.*"],
-    build: {
-        emptyOutDir: true,
-        outDir: "../dist"
-    }
-})
+  root: "src",
+  assetsInclude: ["./assets/**/*.*"],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        nested: resolve(__dirname, "adopt/index.html"),
+      },
+    },
+
+    emptyOutDir: true,
+    outDir: "../dist",
+  },
+});
